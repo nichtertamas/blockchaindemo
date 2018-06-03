5fc4deb382276a0d9a316939dcd73b2ad81c83f4
Nichter Tamas <nichtertamas@gmail.com>
2018-06-03 21:47:22 +0200

    transaction pool and block hashing implemented

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,8 @@
+const sha256 = require('sha256');
+
 function Blockchain() {
 	this.chain = [];
-	this.newTransactions = [];
+	this.pendingTransactions = [];
 }
 
 
@@ -9,16 +11,39 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
 	const newBlock = {
 		index: this.chain.length + 1,
 		timestamp: Date.now(),
-		transactions:this.newTransactions,
-		nonce:nonce,
-		hash:hash,
+		transactions: this.pendingTransactions,
+		nonce: nonce,
+		hash: hash,
 		previousBlockHash: previousBlockHash
 	};
 
-	this.newTransactions = [];
-	this.chain.push(newBlock)
+	this.pendingTransactions = [];
+	this.chain.push(newBlock);
 
 	return newBlock;
 }
+
+Blockchain.prototype.getLastBlock = function() {	
+	return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+	const newTransaction = {
+		amount: amount,
+		sender: sender,
+		recipient: recipient
+	};
+
+	this.pendingTransactions.push(newTransaction);
+
+	return this.getLastBlock()['index']+1;
+}
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+	const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+	const hash = sha256(dataAsString);
+	return hash;	
+}
+
 
 module.exports = Blockchain;
